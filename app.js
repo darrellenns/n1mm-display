09eb697016584308935269bc1420542c2ac6319e
Darrell Enns <darrell@darrellenns.com>
2015-06-12 09:49:58 -0700

    add logging, general cleanup

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var logger = require('morgan');
+app.use(logger('dev'));
+
 server.listen(3000);
+server.on('listening',function(){
+	//let the user know the server is listening
+	var addr = server.address();
+	var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+	console.log("Listening on "+bind);
+});
 
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
@@ -22,3 +33,34 @@ var testtimer= function () {
 	setTimeout(testtimer,1000);
 }
 testtimer();
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
